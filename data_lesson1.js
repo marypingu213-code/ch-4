95091ec1a6cc9b8cb2cc957522f71d360fefa6bb
Carz <marypingu213@gmail.com>
2026-07-30 12:57:44 +0800

    Update data_lesson1.js

--- data_lesson1.js
+++ data_lesson1.js
@@ -1,37 +1,52 @@
 // 檔案名稱：data_lesson1.js
-// 第一關：部件組合大挑戰 - 完整題庫資料
+// 第一關：部件組合大挑戰 - 完整題庫資料 (依課次分類)
 
-const lessonData = [
+const lessonData = {
     // 第一課
-    { char: "串", components: ["lesson1/L1/筆順-串-部件-1.png", "lesson1/L1/筆順-串-部件-2.png", "lesson1/L1/筆順-串-部件-3.png"], radical: "丨", radicalChoices: ["丨", "口", "人", "木"] },
-    { char: "泳", components: ["lesson1/L1/筆順-泳-部件-1.png", "lesson1/L1/筆順-泳-部件-2.png"], radical: "水", radicalChoices: ["水", "木", "人", "日"] },
-    { char: "陀", components: ["lesson1/L1/筆順-陀-部件-1.png", "lesson1/L1/筆順-陀-部件-2.png"], radical: "阜", radicalChoices: ["阜", "宀", "匕", "手"] },
-    { char: "姿", components: ["lesson1/L1/筆順-姿-部件-1.png", "lesson1/L1/筆順-姿-部件-2.png", "lesson1/L1/筆順-姿-部件-3.png"], radical: "女", radicalChoices: ["女", "欠", "次", "人"] },
-    { char: "般", components: ["lesson1/L1/筆順-般-部件-1.png", "lesson1/L1/筆順-般-部件-2.png", "lesson1/L1/筆順-般-部件-3.png"], radical: "舟", radicalChoices: ["舟", "几", "又", "水"] },
-    { char: "溜", components: ["lesson1/L1/筆順-溜-部件-1.png", "lesson1/L1/筆順-溜-部件-2.png", "lesson1/L1/筆順-溜-部件-3.png", "lesson1/L1/筆順-溜-部件-4.png"], radical: "水", radicalChoices: ["水", "刀", "田", "木"] },
-    { char: "滑", components: ["lesson1/L1/筆順-滑-部件-1.png", "lesson1/L1/筆順-滑-部件-2.png", "lesson1/L1/筆順-滑-部件-3.png"], radical: "水", radicalChoices: ["水", "月", "骨", "手"] },
-    { char: "煩", components: ["lesson1/L1/筆順-煩-部件-1.png", "lesson1/L1/筆順-煩-部件-2.png", "lesson1/L1/筆順-煩-部件-3.png"], radical: "火", radicalChoices: ["火", "貝", "一", "頁"] },
-    { char: "緩", components: ["lesson1/L1/筆順-緩-部件-1.png", "lesson1/L1/筆順-緩-部件-2.png", "lesson1/L1/筆順-緩-部件-3.png", "lesson1/L1/筆順-緩-部件-4.png"], radical: "糸", radicalChoices: ["糸", "又", "爪", "厂"] },
-    { char: "遨", components: ["lesson1/L1/筆順-遨-部件-1.png", "lesson1/L1/筆順-遨-部件-2.png", "lesson1/L1/筆順-遨-部件-3.png", "lesson1/L1/筆順-遨-部件-4.png"], radical: "辵", radicalChoices: ["辵", "士", "方", "攵"] },
-    { char: "螺", components: ["lesson1/L1/筆順-螺-部件-1.png", "lesson1/L1/筆順-螺-部件-2.png", "lesson1/L1/筆順-螺-部件-3.png"], radical: "虫", radicalChoices: ["虫", "田", "糸", "木"] },
-    { char: "轉", components: ["lesson1/L1/筆順-轉-部件-1.png", "lesson1/L1/筆順-轉-部件-2.png", "lesson1/L1/筆順-轉-部件-3.png"], radical: "車", radicalChoices: ["車", "寸", "專", "手"] },
-    { char: "躍", components: ["lesson1/L1/筆順-躍-部件-1.png", "lesson1/L1/筆順-躍-部件-2.png", "lesson1/L1/筆順-躍-部件-3.png"], radical: "足", radicalChoices: ["足", "羽", "隹", "鳥"] },
-    { char: "鷹", components: ["lesson1/L1/筆順-鷹-部件-1.png", "lesson1/L1/筆順-鷹-部件-2.png", "lesson1/L1/筆順-鷹-部件-3.png", "lesson1/L1/筆順-鷹-部件-4.png"], radical: "鳥", radicalChoices: ["鳥", "广", "人", "隹"] },
+    "lesson1": [
+        { char: "串", components: ["lesson1/L1/筆順-串-部件-1.png", "lesson1/L1/筆順-串-部件-2.png", "lesson1/L1/筆順-串-部件-3.png"], radical: "丨", radicalChoices: ["丨", "口", "人", "木"] },
+        { char: "泳", components: ["lesson1/L1/筆順-泳-部件-1.png", "lesson1/L1/筆順-泳-部件-2.png"], radical: "水", radicalChoices: ["水", "木", "人", "日"] },
+        { char: "陀", components: ["lesson1/L1/筆順-陀-部件-1.png", "lesson1/L1/筆順-陀-部件-2.png"], radical: "阜", radicalChoices: ["阜", "宀", "匕", "手"] },
+        { char: "姿", components: ["lesson1/L1/筆順-姿-部件-1.png", "lesson1/L1/筆順-姿-部件-2.png", "lesson1/L1/筆順-姿-部件-3.png"], radical: "女", radicalChoices: ["女", "欠", "次", "人"] },
+        { char: "般", components: ["lesson1/L1/筆順-般-部件-1.png", "lesson1/L1/筆順-般-部件-2.png", "lesson1/L1/筆順-般-部件-3.png"], radical: "舟", radicalChoices: ["舟", "几", "又", "水"] },
+        { char: "溜", components: ["lesson1/L1/筆順-溜-部件-1.png", "lesson1/L1/筆順-溜-部件-2.png", "lesson1/L1/筆順-溜-部件-3.png", "lesson1/L1/筆順-溜-部件-4.png"], radical: "水", radicalChoices: ["水", "刀", "田", "木"] },
+        { char: "滑", components: ["lesson1/L1/筆順-滑-部件-1.png", "lesson1/L1/筆順-滑-部件-2.png", "lesson1/L1/筆順-滑-部件-3.png"], radical: "水", radicalChoices: ["水", "月", "骨", "手"] },
+        { char: "煩", components: ["lesson1/L1/筆順-煩-部件-1.png", "lesson1/L1/筆順-煩-部件-2.png", "lesson1/L1/筆順-煩-部件-3.png"], radical: "火", radicalChoices: ["火", "貝", "一", "頁"] },
+        { char: "緩", components: ["lesson1/L1/筆順-緩-部件-1.png", "lesson1/L1/筆順-緩-部件-2.png", "lesson1/L1/筆順-緩-部件-3.png", "lesson1/L1/筆順-緩-部件-4.png"], radical: "糸", radicalChoices: ["糸", "又", "爪", "厂"] },
+        { char: "邀", components: ["lesson1/L1/筆順-邀-部件-1.png", "lesson1/L1/筆順-邀-部件-2.png", "lesson1/L1/筆順-邀-部件-3.png", "lesson1/L1/筆順-邀-部件-4.png"], radical: "辵", radicalChoices: ["辵", "士", "方", "攵"] },
+        { char: "螺", components: ["lesson1/L1/筆順-螺-部件-1.png", "lesson1/L1/筆順-螺-部件-2.png", "lesson1/L1/筆順-螺-部件-3.png"], radical: "虫", radicalChoices: ["虫", "田", "糸", "木"] },
+        { char: "轉", components: ["lesson1/L1/筆順-轉-部件-1.png", "lesson1/L1/筆順-轉-部件-2.png", "lesson1/L1/筆順-轉-部件-3.png"], radical: "車", radicalChoices: ["車", "寸", "專", "手"] },
+        { char: "躍", components: ["lesson1/L1/筆順-躍-部件-1.png", "lesson1/L1/筆順-躍-部件-2.png", "lesson1/L1/筆順-躍-部件-3.png"], radical: "足", radicalChoices: ["足", "羽", "隹", "鳥"] },
+        { char: "鷹", components: ["lesson1/L1/筆順-鷹-部件-1.png", "lesson1/L1/筆順-鷹-部件-2.png", "lesson1/L1/筆順-鷹-部件-3.png", "lesson1/L1/筆順-鷹-部件-4.png"], radical: "鳥", radicalChoices: ["鳥", "广", "人", "隹"] }
+    ],
 
     // 第二課
-    { char: "束", components: ["lesson1/L2/筆順-束-部件-1.png", "lesson1/L2/筆順-束-部件-2.png"], radical: "木", radicalChoices: ["木", "口", "人", "水"] },
-    { char: "依", components: ["lesson1/L2/筆順-依-部件-1.png", "lesson1/L2/筆順-依-部件-2.png"], radical: "人", radicalChoices: ["人", "衣", "木", "手"] },
-    { char: "姐", components: ["lesson1/L2/筆順-姐-部件-1.png", "lesson1/L2/筆順-姐-部件-2.png"], radical: "女", radicalChoices: ["女", "且", "木", "人"] },
-    { char: "幸", components: ["lesson1/L2/筆順-幸-部件-1.png", "lesson1/L2/筆順-幸-部件-2.png", "lesson1/L2/筆順-幸-部件-3.png"], radical: "干", radicalChoices: ["干", "土", "十", "手"] },
-    { char: "拋", components: ["lesson1/L2/筆順-拋-部件-1.png", "lesson1/L2/筆順-拋-部件-2.png", "lesson1/L2/筆順-拋-部件-3.png"], radical: "手", radicalChoices: ["手", "力", "人", "水"] },
-    { char: "招", components: ["lesson1/L2/筆順-招-部件-1.png", "lesson1/L2/筆順-招-部件-2.png", "lesson1/L2/筆順-招-部件-3.png"], radical: "手", radicalChoices: ["手", "刀", "口", "木"] },
-    { char: "爭", components: ["lesson1/L2/筆順-爭-部件-1.png", "lesson1/L2/筆順-爭-部件-2.png"], radical: "爪", radicalChoices: ["爪", "手", "口", "木"] },
-    { char: "則", components: ["lesson1/L2/筆順-則-部件-1.png", "lesson1/L2/筆順-則-部件-2.png"], radical: "刀", radicalChoices: ["刀", "貝", "人", "水"] },
-    { char: "偷", components: ["lesson1/L2/筆順-偷-部件-1.png", "lesson1/L2/筆順-偷-部件-2.png", "lesson1/L2/筆順-偷-部件-3.png", "lesson1/L2/筆順-偷-部件-4.png", "lesson1/L2/筆順-偷-部件-5.png"], radical: "人", radicalChoices: ["人", "刀", "月", "一"] },
-    { char: "捨", components: ["lesson1/L2/筆順-捨-部件-1.png", "lesson1/L2/筆順-捨-部件-2.png", "lesson1/L2/筆順-捨-部件-3.png"], radical: "手", radicalChoices: ["手", "人", "舌", "木"] },
-    { char: "湯", components: ["lesson1/L2/筆順-湯-部件-1.png", "lesson1/L2/筆順-湯-部件-2.png", "lesson1/L2/筆順-湯-部件-3.png", "lesson1/L2/筆順-湯-部件-4.png"], radical: "水", radicalChoices: ["水", "日", "一", "手"] },
-    { char: "暫", components: ["lesson1/L2/筆順-暫-部件-1.png", "lesson1/L2/筆順-暫-部件-2.png", "lesson1/L2/筆順-暫-部件-3.png"], radical: "日", radicalChoices: ["日", "車", "斤", "木"] },
-    { char: "糧", components: ["lesson1/L2/筆順-糧-部件-1.png", "lesson1/L2/筆順-糧-部件-2.png", "lesson1/L2/筆順-糧-部件-3.png", "lesson1/L2/筆順-糧-部件-4.png"], radical: "米", radicalChoices: ["米", "日", "一", "里"] },
-    { char: "繩", components: ["lesson1/L2/筆順-繩-部件-1.png", "lesson1/L2/筆順-繩-部件-2.png", "lesson1/L2/筆順-繩-部件-3.png", "lesson1/L2/筆順-繩-部件-4.png"], radical: "糸", radicalChoices: ["糸", "口", "虫", "水"] },
-    { char: "警", components: ["lesson1/L2/筆順-警-部件-1.png", "lesson1/L2/筆順-警-部件-2.png"], radical: "言", radicalChoices: ["言", "敬", "口", "木"] }
-];
+    "lesson2": [
+        { char: "束", components: ["lesson1/L2/筆順-束-部件-1.png", "lesson1/L2/筆順-束-部件-2.png"], radical: "木", radicalChoices: ["木", "口", "人", "水"] },
+        { char: "依", components: ["lesson1/L2/筆順-依-部件-1.png", "lesson1/L2/筆順-依-部件-2.png"], radical: "人", radicalChoices: ["人", "衣", "木", "手"] },
+        { char: "姐", components: ["lesson1/L2/筆順-姐-部件-1.png", "lesson1/L2/筆順-姐-部件-2.png"], radical: "女", radicalChoices: ["女", "且", "木", "人"] },
+        { char: "幸", components: ["lesson1/L2/筆順-幸-部件-1.png", "lesson1/L2/筆順-幸-部件-2.png", "lesson1/L2/筆順-幸-部件-3.png"], radical: "干", radicalChoices: ["干", "土", "十", "手"] },
+        { char: "拋", components: ["lesson1/L2/筆順-拋-部件-1.png", "lesson1/L2/筆順-拋-部件-2.png", "lesson1/L2/筆順-拋-部件-3.png"], radical: "手", radicalChoices: ["手", "力", "人", "水"] },
+        { char: "招", components: ["lesson1/L2/筆順-招-部件-1.png", "lesson1/L2/筆順-招-部件-2.png", "lesson1/L2/筆順-招-部件-3.png"], radical: "手", radicalChoices: ["手", "刀", "口", "木"] },
+        { char: "爭", components: ["lesson1/L2/筆順-爭-部件-1.png", "lesson1/L2/筆順-爭-部件-2.png"], radical: "爪", radicalChoices: ["爪", "手", "口", "木"] },
+        { char: "則", components: ["lesson1/L2/筆順-則-部件-1.png", "lesson1/L2/筆順-則-部件-2.png"], radical: "刀", radicalChoices: ["刀", "貝", "人", "水"] },
+        { char: "偷", components: ["lesson1/L2/筆順-偷-部件-1.png", "lesson1/L2/筆順-偷-部件-2.png", "lesson1/L2/筆順-偷-部件-3.png", "lesson1/L2/筆順-偷-部件-4.png", "lesson1/L2/筆順-偷-部件-5.png"], radical: "人", radicalChoices: ["人", "刀", "月", "一"] },
+        { char: "捨", components: ["lesson1/L2/筆順-捨-部件-1.png", "lesson1/L2/筆順-捨-部件-2.png", "lesson1/L2/筆順-捨-部件-3.png"], radical: "手", radicalChoices: ["手", "人", "舌", "木"] },
+        { char: "湯", components: ["lesson1/L2/筆順-湯-部件-1.png", "lesson1/L2/筆順-湯-部件-2.png", "lesson1/L2/筆順-湯-部件-3.png", "lesson1/L2/筆順-湯-部件-4.png"], radical: "水", radicalChoices: ["水", "日", "一", "手"] },
+        { char: "暫", components: ["lesson1/L2/筆順-暫-部件-1.png", "lesson1/L2/筆順-暫-部件-2.png", "lesson1/L2/筆順-暫-部件-3.png"], radical: "日", radicalChoices: ["日", "車", "斤", "木"] },
+        { char: "糧", components: ["lesson1/L2/筆順-糧-部件-1.png", "lesson1/L2/筆順-糧-部件-2.png", "lesson1/L2/筆順-糧-部件-3.png", "lesson1/L2/筆順-糧-部件-4.png"], radical: "米", radicalChoices: ["米", "日", "一", "里"] },
+        { char: "繩", components: ["lesson1/L2/筆順-繩-部件-1.png", "lesson1/L2/筆順-繩-部件-2.png", "lesson1/L2/筆順-繩-部件-3.png", "lesson1/L2/筆順-繩-部件-4.png"], radical: "糸", radicalChoices: ["糸", "口", "虫", "水"] },
+        { char: "警", components: ["lesson1/L2/筆順-警-部件-1.png", "lesson1/L2/筆順-警-部件-2.png"], radical: "言", radicalChoices: ["言", "敬", "口", "木"] }
+    ],
+
+    "lesson3": [],
+    "lesson4": [],
+    "lesson5": [],
+    "lesson6": [],
+    "lesson7": [],
+    "lesson8": [],
+    "lesson9": [],
+    "lesson10": [],
+    "lesson11": [],
+    "lesson12": []
+};
